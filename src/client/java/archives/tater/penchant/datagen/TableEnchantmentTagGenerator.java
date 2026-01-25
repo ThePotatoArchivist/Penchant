662fe73718c95e893f5be4cf8e13597c7b10d7c0
ThePotatoArchivist <josiahfu@gmail.com>
2026-01-25 02:43:08 -0800

    more loot tweaks

--- src/client/java/archives/tater/penchant/datagen/TableEnchantmentTagGenerator.java
+++ src/client/java/archives/tater/penchant/datagen/TableEnchantmentTagGenerator.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -21,31 +23,62 @@ public class TableEnchantmentTagGenerator extends FabricTagProvider<Enchantment>
 
     @Override
     protected void addTags(HolderLookup.Provider wrapperLookup) {
-        var normalTreasure = TagKey.create(Registries.ENCHANTMENT, Penchant.id("normal_treasure"));
+        var rare = TagKey.create(Registries.ENCHANTMENT, Penchant.id("rare"));
+        var uncommon = TagKey.create(Registries.ENCHANTMENT, Penchant.id("uncommon"));
 
         //noinspection unchecked
-        builder(normalTreasure).add(
+        builder(rare).add(
                 Enchantments.FROST_WALKER,
                 Enchantments.FIRE_ASPECT,
                 Enchantments.FLAME,
                 Enchantments.SILK_TOUCH,
+                Enchantments.FORTUNE,
                 Enchantments.LUNGE,
                 Enchantments.CHANNELING,
                 Enchantments.RIPTIDE,
-                Enchantments.FORTUNE,
                 Enchantments.THORNS,
-                Enchantments.INFINITY,
-                Enchantments.RESPIRATION
+                Enchantments.INFINITY
         );
+
+        //noinspection unchecked
+        builder(uncommon).add(
+                Enchantments.RESPIRATION,
+                Enchantments.AQUA_AFFINITY,
+                Enchantments.DEPTH_STRIDER,
+                Enchantments.FEATHER_FALLING,
+                Enchantments.FIRE_PROTECTION,
+                Enchantments.BLAST_PROTECTION,
+                Enchantments.PROJECTILE_PROTECTION,
+                Enchantments.SMITE,
+                Enchantments.BANE_OF_ARTHROPODS,
+                Enchantments.SWEEPING_EDGE,
+                Enchantments.KNOCKBACK,
+                Enchantments.PUNCH,
+                Enchantments.MULTISHOT,
+                Enchantments.DENSITY,
+                Enchantments.BREACH,
+                Enchantments.LOOTING,
+                Enchantments.LUCK_OF_THE_SEA
+        )
+                .addOptional(ResourceKey.create(Registries.ENCHANTMENT, Identifier.fromNamespaceAndPath("farmersdelight", "backstabbing")));
+
+        builder(EnchantmentTags.TREASURE)
+                .addTag(rare);
+        builder(EnchantmentTags.NON_TREASURE)
+                .tagex_excludeTag(rare);
         builder(EnchantmentTags.IN_ENCHANTING_TABLE)
-                .tagex_excludeTag(normalTreasure);
+                .tagex_excludeTag(uncommon)
+                .tagex_excludeTag(rare);
         builder(EnchantmentTags.TRADEABLE)
-                .addTag(normalTreasure);
+                .tagex_forceExcludeTag(EnchantmentTags.IN_ENCHANTING_TABLE)
+                .tagex_forceExcludeTag(EnchantmentTags.CURSE)
+                .addTag(uncommon);
         builder(EnchantmentTags.ON_RANDOM_LOOT)
-                .addTag(normalTreasure);
-        builder(EnchantmentTags.ON_TRADED_EQUIPMENT)
-                .addTag(normalTreasure);
+                .tagex_forceExcludeTag(EnchantmentTags.NON_TREASURE)
+                .addTag(uncommon)
+                .addTag(rare);
         builder(EnchantmentTags.ON_MOB_SPAWN_EQUIPMENT)
-                .addTag(normalTreasure);
+                .addTag(uncommon)
+                .addTag(rare);
     }
 }
